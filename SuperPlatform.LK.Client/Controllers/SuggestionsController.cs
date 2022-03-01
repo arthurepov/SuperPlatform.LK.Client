79bdddd632737b452b0c41e40d93a45c2be4f6c8
arthurepov <arthurepov@gmail.com>
2022-03-01 20:28:07 +0300

    Переделал модель при поиске по секциям

--- SuperPlatform.LK.Client/Controllers/SuggestionsController.cs
+++ SuperPlatform.LK.Client/Controllers/SuggestionsController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 using SuperPlatform.LK.Client.Domain.Disciplines.Services;
 using SuperPlatform.LK.Client.Domain.Organizations.Services;
 using SuperPlatform.LK.Client.Domain.Sections.Services;
+using SuperPlatform.LK.Client.Extensions;
+using SuperPlatform.LK.Client.Models.Sections;
 using SuperPlatform.LK.Client.Models.Suggestions;
 using System;
 using System.Collections.Generic;
@@ -26,16 +28,32 @@ namespace SuperPlatform.LK.Client.Controllers
 
         private readonly ISectionService _sectionService;
 
+        private readonly ISectionGroupService _sectionGroupService;
+
+        private readonly ISectionGroupChildrenService _sectionGroupChildrenService;
+
+        private readonly ISectionGroupScheduleService _sectionGroupScheduleService;
+
+        private readonly ISectionGroupScheduleTimeService _sectionGroupScheduleTimeService;
+
         public SuggestionsController(
             IMapper mapper,
             IDisciplineService disciplineService,
             IOrganizationService organizationService,
-            ISectionService sectionService)
+            ISectionService sectionService,
+            ISectionGroupService sectionGroupService,
+            ISectionGroupChildrenService sectionGroupChildrenService,
+            ISectionGroupScheduleService sectionGroupScheduleService,
+            ISectionGroupScheduleTimeService sectionGroupScheduleTimeService)
         {
             _mapper = mapper;
             _disciplineService = disciplineService;
             _organizationService = organizationService;
             _sectionService = sectionService;
+            _sectionGroupService = sectionGroupService;
+            _sectionGroupChildrenService = sectionGroupChildrenService;
+            _sectionGroupScheduleService = sectionGroupScheduleService;
+            _sectionGroupScheduleTimeService = sectionGroupScheduleTimeService;
         }
 
         /// <summary>
@@ -44,7 +62,7 @@ namespace SuperPlatform.LK.Client.Controllers
         /// <param name="query"></param>
         /// <returns></returns>
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<SuggestionSectionDto>>> Suggestion(string query)
+        public async Task<ActionResult<IReadOnlyList<SectionDetailsDto>>> Suggestion(string query)
         {
             if (query.Length < 3)
             {
@@ -53,9 +71,51 @@ namespace SuperPlatform.LK.Client.Controllers
 
             var sections = await _sectionService.Suggestion(query);
 
-            var SuggestionSections = _mapper.Map<IReadOnlyList<SuggestionSectionDto>>(sections);
+            var sectionGroups = await _sectionGroupService.GetBySections(sections.Select(x=>x.Id).ToArray());
+            var sectionGroupChildren = await _sectionGroupChildrenService.GetBySectionGroups(sectionGroups.Select(x => x.Id).ToArray());
+            var sectionGroupSchedules = await _sectionGroupScheduleService.GetBySectionGroups(sectionGroups.Select(x => x.Id).ToArray());
+            var sectionGroupScheduleTimes = await _sectionGroupScheduleTimeService.GetBySectionGroupSchedules(sectionGroupSchedules.Select(x => x.Id).ToArray());
 
-            return Ok(SuggestionSections);
+            var model = sections.Select(section => new SectionDetailsDto
+            {
+                Id = section.Id,
+                Name = section.Name,
+                RecordType = section.RecordType.HasValue ? (int?)section.RecordType : null,
+                Address = section.Organization.address,
+                MinAge = section.MinAge,
+                MaxAge = section.MaxAge,
+                OrganizationName = section.Organization.name,
+                Longitude = section.Organization.longitude,
+                Latitude = section.Organization.latitude,
+                Cost = section.Cost,
+                Duration = section.Duration,
+                MaxPlacesCount = section.MaxPlacesCount,
+                MinPlacesCount = section.MinPlacesCount,
+                SectionGroups = sectionGroups
+                    .Select(x => new SectionGroupDto
+                    {
+                        Id = x.Id,
+                        Name = x.Name,
+                        SectionGroupSchedules = sectionGroupSchedules.Where(y => y.SectionGroup.Id == x.Id).Select(y => new SectionGroupScheduleDto
+                        {
+                            Id = y.Id,
+                            DayOfWeek = y.DayOfWeek.GetNumberValue(),
+                            SectionGroupScheduleTimes = sectionGroupScheduleTimes.Where(z => z.SectionGroupSchedule.Id == y.Id).Select(z => new SectionGroupScheduleTimeDto
+                            {
+                                Id = z.Id,
+                                StartTime = z.StartTime,
+                                EndTime = z.EndTime
+                            })
+                            .OrderBy(x => x.StartTime)
+                            .ToList()
+                        })
+                        .OrderBy(x => x.DayOfWeek)
+                        .ToList()
+                    })
+                    .ToList()
+            });
+
+            return Ok(model);
         }
     }
 }
